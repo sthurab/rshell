3a325c84acd9ef361d4e4d9a5dd2a07eeabefec0
sthurab <sthurab@users.noreply.github.com>
2016-11-26 00:21:51 -0800

    Update test_test.sh

--- tests/test_test.sh
+++ tests/test_test.sh
@@ -1,7 +1,8 @@
 #tests for the test command
 
-test -e orOp.cpp
+test -e andOp.cpp && echo "path exists"
+test -e orOp.cpp && echo "path exists"
+test -f andOp.cpp && echo "it is a  regular file"
+test -f orOp.cpp && echo "it is a  regular file"
 
-test -f orOp.cpp
-
-test -d orOp.cpp
+test -d orOp.cpp && echo "it is not a directory"
