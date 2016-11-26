5b8bfa23989b5ae7802606e9a94d517660dfb4f3
sthurab <sthurab@users.noreply.github.com>
2016-11-26 00:18:48 -0800

    Update precedence_test.sh

--- tests/precedence_test.sh
+++ tests/precedence_test.sh
@@ -1,6 +1,8 @@
 #tests for precedence operators
 
 (echo A && echo B) || (echo C && echo D)
+echo A && echo B || echo C && echo D
+
 (echo A) && (echo B) && (echo C) || (echo D)
 ((echo A && echo B) || (echo C))
 (echo A) || ((echo B) || (echo C))
