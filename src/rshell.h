023d78bea18f941e562dd6ad18eac9aaaae8e871
sthurab <sthurab@users.noreply.github.com>
2016-11-25 21:49:47 -0800

    Update rshell.h

--- src/rshell.h
+++ src/rshell.h
@@ -1,11 +1,11 @@
 #ifndef RSHELL_H
 #define RSHELL_H
 
-class rshell {	
-	private:
-		string user_input_str;
-	public:
-		rshell();
+class rshell {
+    private:
+        string input_inc;
+    public:
+        rshell();
 };
 
 #endif
