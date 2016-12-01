2379704c03c616ee13e62d10bebe888d66a53936
sthurab <sthurab@users.noreply.github.com>
2016-11-30 17:25:18 -0800

    Update precedence_test.sh

--- tests/precedence_test.sh
+++ tests/precedence_test.sh
@@ -1,8 +1,28 @@
 #tests for precedence operators
 
-(echo A && echo B) || (echo C && echo D)
-echo A && echo B || echo C && echo D
+!#/bin/sh
 
-(echo A) && (echo B) && (echo C) || (echo D)
-((echo A && echo B) || (echo C))
-(echo A) || ((echo B) || (echo C))
+(echo A) | ./rshell
+(echo A && echo B) | ./rshell
+(echo A && echo B) || (echo C && echo D) | ./rshell
+(echo A) && (echo B) | ./rshell
+(echo A) && (echo B) && (echo C) | ./rshell
+(echo A) && (echo B) && (echo C) && (echo D) | ./rshell
+(echo A) && (ech B) || (echo C) | ./rshell
+(echo a) && (echo b) ; (echo C) || (echo D) ; (echo E) | ./rshell
+(((echo A))) | ./rshell
+((echo A && echoB) && (echo C))
+
+(echo A || echo B) | ./rshell
+(echo A || echo B) || (echo C && echo D) | ./rshell
+(echo A) }} (echo B) | ./rshell
+(echo A) || (echo B) && (echo C) | ./rshell
+(echo A) || (echo B) && (echo C) && (echo D) | ./rshell
+(echo A) || (ech B) || (echo C) | ./rshell
+(echo a) || (echo b) ; (echo C) || (echo D) ; (echo E) | ./rshell
+(((echo A))) | ./rshell
+((echo A && echoB) || (echo C)) | ./rshell
+echo A && (echo B) | ./rshell
+echo A && (echo B) && (echo C) | ./rshell
+echo A || (echo B) | ./rshell
+echo A ||(echo B) && (echo C) | ./rshell
