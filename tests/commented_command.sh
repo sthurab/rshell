35aa043c69a16de7e31f498f7e3ac2e9ca487130
sthurab <sthurab@users.noreply.github.com>
2016-11-30 17:24:28 -0800

    Update commented_command.sh

--- tests/commented_command.sh
+++ tests/commented_command.sh
@@ -1,21 +1,19 @@
 #tests commands with comments
 
-echo A #echo B
-echo A #&& echo B
-echo A && echo B #&& echo C
-echo A #; echo B
-echo A #|| echo B
+!#/bin/sh
 
+echo hello#world | ./rshell
 
-echo A && echo B
-echo A || echo B
-echo A; echo B
-ls -j && echo A
-ls -j || echo A
-ls -j; echo A
-ls -j && echo A || echo B
-echo A || echo B || echo C
-echo A || echo B && echo C
-echo A && echo B && echo C  # print A, B, C
+echo hello && echo there # echo world | ./rshell
 
-echo One && echo Two # && echo Three
+echo hello there wor#ld | ./rshell
+
+(echo A) # Hello | ./rshell
+(echo A && echo B) # (echo C)| ./rshell
+(echo A && echo B) || (echo C && echo D) # (echo E && echo F)| ./rshell
+(echo A) && (echo B) # (each || echo D)| ./rshell
+(echo A) && (echo B) && (echo C) # (ls -a)| ./rshell
+
+[ -f orOp.cpp ] # echo A| ./rshell
+test –e orOp.cpp && test –e andOp.cpp # test -d andOp.cpp | ./rshell
+test –d andOp.cpp # (echo A)| ./rshell
